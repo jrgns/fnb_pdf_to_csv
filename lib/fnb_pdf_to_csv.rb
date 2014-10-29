cc37b345ab0f5f7134bdc598fb1a2189cafbf62b
Jurgens du Toit <jrgns@jadeit.co.za>
2014-10-29 22:35:47 +0200

    Imitate the FNB statement transaction section

--- lib/fnb_pdf_to_csv.rb
+++ lib/fnb_pdf_to_csv.rb
@@ -22,7 +22,26 @@ class FnbPdfToCsv
 
   def output file
     f = File.new file, 'w'
-    lines.each { |line| f.write line }
+    lines.each { |line| f.write line.to_csv }
+  end
+
+  def statement file
+    f = File.new file, 'w'
+    count = 1
+    lines.each do |line|
+      f.write statement_line(line, count).to_csv
+      count = count + 1
+    end
+  end
+
+  def statement_line line, count
+    line.insert(0, 5)
+    line.insert(1, count)
+    line[2] = "'#{line[2]}'"
+    line.insert(5, '') if line.count == 7
+    line[6] = 0 - line[6].to_f if line[7].nil?
+    line.delete_at(7)
+    line
   end
 
   def parse_page page
@@ -31,7 +50,7 @@ class FnbPdfToCsv
 
   def parse_line line
     line.match(/(\d{2} \w{3})\s{1,2}(.*?)([0-9 ]+\.\d{2})(Cr)?$/) do |m|
-      @lines.push mangle_line(m.to_a).to_csv
+      @lines.push mangle_line(m.to_a)
     end
   end
 
