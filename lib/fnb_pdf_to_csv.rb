13346bb88a8d7afa6f47c9e6f5eaa7b8cb50706e
Jurgens du Toit <jrgns@jadeit.co.za>
2015-02-25 21:44:26 +0200

    Refined the line matching

--- lib/fnb_pdf_to_csv.rb
+++ lib/fnb_pdf_to_csv.rb
@@ -1,9 +1,12 @@
-#require 'fnb_pdf_to_csv/version'
+require 'version'
 require 'pdf-reader'
 require 'csv'
 
 class FnbPdfToCsv
   attr_reader :lines
+
+  AMOUNT = '\(?[0-9][0-9,]*\.[0-9]{2}\)?(Cr)?'
+  DATE   = '\d{2} \w{3}'
 
   def initialize file
     @reader = ::PDF::Reader.new file
@@ -38,9 +41,6 @@ class FnbPdfToCsv
     line.insert(0, 5)
     line.insert(1, count)
     line[2] = "'#{line[2]}'"
-    line.insert(5, '') if line.count == 7
-    line[6] = 0 - line[6].to_f if line[7].nil?
-    line.delete_at(7)
     line
   end
 
@@ -49,18 +49,48 @@ class FnbPdfToCsv
   end
 
   def parse_line line
-    line.match(/(\d{2} \w{3})\s{1,2}(.*?)([0-9 ]+\.\d{2})(Cr)?$/) do |m|
-      @lines.push mangle_line(m.to_a)
+    #puts line
+    line.match(/^\s*(#{DATE})(.*?)(#{AMOUNT})\s+(#{AMOUNT})(\s+#{AMOUNT})?$/) do |m|
+
+      @lines.push clean_line(mangle_line(m.to_a))
     end
   end
 
+  def clean_date(date)
+    day, month = date.split(/\s/)
+    Time.new(Time.new.year, month, day.to_i).strftime("%Y-%m-%d")
+  end
+
+  def clean_amount(amount)
+    return amount if amount.nil?
+    return 0 - amount[1..-2].to_f if amount[0] == '(' and amount[-1] == ')'
+    if amount[-2..-1] == 'Cr'
+      return amount[0..-3].to_f
+    else
+      return 0 - amount.to_f
+    end
+  end
+
+  def clean_line(line)
+    line[0] = clean_date line[0]
+    line[4] = clean_amount line[4]
+    line[5] = clean_amount line[5]
+    line[6] = clean_amount line[6]
+
+    line
+  end
+
   def mangle_line arr
-    arr[2] = arr[2].split(/\s{2,}/)
-    arr.insert(3, arr[2][1])
-    arr[2] = arr[2][0]
     arr.delete_at 0
-    arr.map! { |elm| elm.strip unless elm.nil? }
-    arr[3] = arr[3].sub(/\s/, '') unless arr[3].nil?
+    arr.map! { |elm| elm.strip unless elm.nil? } # Cleanup
+
+    arr.delete_at 3
+    arr.delete_at 4
+    arr.delete_at 5
+    arr[1] = arr[1].split(/\s{2,}/) # We get the three descriptions as one string
+    arr.insert(2, arr[1][1])        # So split them up and add them back
+    arr.insert(3, arr[1][2])
+    arr[1] = arr[1][0]
     arr
   end
 end
